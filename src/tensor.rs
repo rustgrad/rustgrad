c70fb55aac21b3528075ad24fd440b747d9170c6
felix <felix@aipieper.de>
2024-12-21 13:14:49 +0100

    Add queue backward
    Priority requires hashing and partially equal, to be able to update queue items

--- src/tensor.rs
+++ src/tensor.rs
@@ -1,6 +1,9 @@
 use ndarray::{Array, IxDyn};
+use priority_queue::PriorityQueue;
 use std::{
     cell::RefCell,
+    cmp::Reverse,
+    hash::{Hash, Hasher},
     ops::{Add, Deref, Mul},
     rc::Rc,
 };
@@ -18,6 +21,20 @@ pub struct Tensor {
     pub container: Rc<RefCell<DataContainer>>,
     prev_op: Option<Operation>,
 }
+impl Hash for Tensor {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        let container_adress = (*self.container).as_ptr() as usize;
+        container_adress.hash(state);
+    }
+}
+impl PartialEq for Tensor {
+    fn eq(&self, other: &Self) -> bool {
+        let data_address = (*self.container).as_ptr() as usize;
+        let other_data_address = (*other.container).as_ptr() as usize;
+        data_address == other_data_address
+    }
+}
+impl Eq for Tensor {}
 
 impl Tensor {
     pub fn new(data: Array<f32, IxDyn>) -> Tensor {
@@ -48,6 +65,43 @@ impl Tensor {
 
         let start_grad = Array::ones(self.shape());
         self.backward_internal(start_grad);
+    }
+    fn add_grad(&self, grad: Array<f32, IxDyn>) {
+        let new_grad = self.grad().unwrap_or(Array::zeros(self.shape())).clone() + grad.clone();
+        self.container.deref().borrow_mut().grad = Some(new_grad);
+        self.container.deref().borrow_mut().num_consumers -= 1;
+    }
+    fn consumers(&self) -> usize {
+        (*self.container).borrow().num_consumers
+    }
+
+    pub fn queue_backward(&mut self) {
+        self.build_graph();
+
+        let start_grad = Array::ones(self.shape());
+        self.queue_backward_internal(start_grad);
+    }
+
+    pub fn queue_backward_internal(&self, grad: Array<f32, IxDyn>) {
+        self.add_grad(grad);
+        let mut queue: PriorityQueue<Tensor, Reverse<usize>> = PriorityQueue::new();
+        queue.push(self.clone(), Reverse(self.consumers()));
+        while !queue.is_empty() {
+            let (tensor, priority) = queue.pop().expect("Queue should not be empty");
+            if priority.0 > 0 {
+                panic!("Did not find ready tensor");
+            }
+            let operation = tensor.prev_op.clone();
+            if operation.is_none() {
+                continue;
+            }
+            let grads = operation.unwrap().grad(&tensor);
+            for (grad, tensor) in grads {
+                tensor.add_grad(grad);
+                let priority = tensor.consumers();
+                queue.push(tensor, Reverse(priority));
+            }
+        }
     }
 
     pub fn backward_internal(&mut self, grad: Array<f32, IxDyn>) {
@@ -129,6 +183,18 @@ enum Operation {
     Mul(TensorMul),
     // MatMul(TensorMatMul<A, D, E>),
 }
+impl Grad for Operation {
+    fn grad(&self, output: &Tensor) -> Vec<(Array<f32, IxDyn>, Tensor)> {
+        match self {
+            Operation::Add(tensor_add) => tensor_add.grad(output),
+            Operation::Mul(tensor_mul) => tensor_mul.grad(output),
+        }
+    }
+}
+
+trait Grad {
+    fn grad(&self, output: &Tensor) -> Vec<(Array<f32, IxDyn>, Tensor)>;
+}
 
 #[derive(Debug, Clone)]
 struct TensorMul {
@@ -145,6 +211,19 @@ impl TensorMul {
             second: Box::new(input_b),
         };
         Tensor::new_with_prev(result, Operation::Mul(node))
+    }
+    pub fn grad(&self, output: &Tensor) -> Vec<(Array<f32, IxDyn>, Tensor)> {
+        let grad = output.container.borrow().grad.clone();
+        let grad = grad.unwrap_or(Array::ones(output.shape()));
+        [self.first.clone(), self.second.clone()]
+            .iter()
+            .map(|tensor| {
+                (
+                    tensor.container.borrow().array.clone() * grad.clone(),
+                    (**tensor).clone(),
+                )
+            })
+            .collect()
     }
     pub fn backward(&mut self, output: &mut Tensor) {
         let grad = output.container.borrow().grad.clone();
@@ -170,6 +249,14 @@ impl TensorAdd {
             second: Box::new(input_b),
         };
         Tensor::new_with_prev(result, Operation::Add(node))
+    }
+    fn grad(&self, output: &Tensor) -> Vec<(Array<f32, IxDyn>, Tensor)> {
+        let maybe_grad = output.container.borrow().grad.clone();
+        let grad = maybe_grad.unwrap_or(ndarray::Array::zeros(output.shape()));
+        [self.first.clone(), self.second.clone()]
+            .iter()
+            .map(|tensor| (grad.clone(), (**tensor).clone()))
+            .collect()
     }
     fn backward(&mut self, output: &mut Tensor) {
         let maybe_grad = output.container.borrow().grad.clone();
@@ -198,6 +285,28 @@ mod tests {
     use ndarray::array;
 
     use super::*;
+
+    #[test]
+    fn it_works_2() {
+        let test_0 = Tensor::new(array![[1.0, 2.0, 3.0, 4.0]].into_dyn()); // grad = 2 * grad_1 = 4 * test_1 = 8 * test_0
+        let test_1 = test_0.clone() + test_0.clone(); // grad_2 = 2 * test_1
+        let mut test_2: Tensor = test_1.clone() * test_1.clone();
+        println!("forward: {:?}", test_2);
+        println!("_____________________________");
+        test_2.queue_backward();
+
+        assert_eq!(
+            test_0.grad(),
+            Some(array![[8.0, 16.0, 24.0, 32.0]].into_dyn())
+        );
+        assert_eq!(
+            test_1.grad(),
+            Some(array![[4.0, 8.0, 12.0, 16.0]].into_dyn())
+        );
+        assert_eq!(test_2.grad(), Some(array![[1.0, 1.0, 1.0, 1.0]].into_dyn()));
+
+        println!("{:?}", test_2);
+    }
 
     #[test]
     fn it_works() {
