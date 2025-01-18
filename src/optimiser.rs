263066a895374c69267eb106be5997ad0e621906
Felix Pieper <felix@aipieper.de>
2025-01-18 18:55:31 +0100

    make function to predict more difficult

--- src/optimiser.rs
+++ src/optimiser.rs
@@ -27,7 +27,8 @@ mod tests {
         nn::{LinearLayer, MLP},
         shape::Shape,
     };
-    use ndarray::array;
+    use ndarray::{array, Array};
+    use num_traits::Pow;
 
     use super::*;
 
@@ -35,21 +36,32 @@ mod tests {
     fn test_optimizer() {
         let mlp = MLP::new(2, 2, 10, 1);
         // let layer = LinearLayer::new(2, 1);
-        let optimiser = SGDOptimizer::new(0.01 as f32, mlp.parameters());
-        let epochs = 100;
+        let optimiser = SGDOptimizer::new(0.001 as f32, mlp.parameters());
+        let epochs = 1000;
         for i in 0..epochs {
             optimiser.zero_grad();
             let input = Tensor::new_random(Shape::new([2]));
             let forwarded = mlp.forward(input.clone());
-            let mut loss = forwarded.clone() * forwarded.clone();
+            let expected_output = input.data();
+            let expected_output = expected_output[0] - expected_output[1];
+            let expected_output = Tensor::new(
+                Array::from_vec(vec![expected_output])
+                    .into_shape_clone(forwarded.shape())
+                    .unwrap(),
+            );
+            let mut loss = -expected_output.clone() + forwarded.clone();
             loss.backward();
             optimiser.step();
+
+            println!("loss {:?}", loss.data());
             if i == epochs - 1 {
+                println!("loss {:?}", loss.data());
                 println!("last forward {:?}", forwarded);
-                println!("layer {:?}", mlp);
+                // println!("layer {:?}", mlp);
                 println!("input {:?}", input);
+                println!("expected_output {:?}", expected_output)
             }
         }
-        println!("{:?}", mlp);
+        // println!("{:?}", mlp);
     }
 }
