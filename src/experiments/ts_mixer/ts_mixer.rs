31697be447134f8b7de6dd7423a0a269880c4d0e
felix <felix@aipieper.de>
2025-06-28 17:15:53 +0200

    aaa

--- src/experiments/ts_mixer/ts_mixer.rs
+++ src/experiments/ts_mixer/ts_mixer.rs
@@ -4,17 +4,35 @@ use crate::{dimensions::Dimension, tensor::Tensor};
 
 use super::MixingLayer;
 
-pub struct TsMixer<L: Dimension, C: Dimension, DOut: Dimension> {
-    layers: Vec<MixingLayer<L, C, C>>,
+pub struct TsMixer<L: Dimension, C: Dimension, DOut: Dimension, const HIDDEN_LAYERS: usize> {
+    layers: [MixingLayer<L, C, C>; HIDDEN_LAYERS],
     last_layer: MixingLayer<L, C, DOut>,
-    output: PhantomData<DOut>,
 }
-
-impl<L: Dimension, C: Dimension, DOut: Dimension> TsMixer<L, C, DOut> {
+impl<L: Dimension, C: Dimension, DOut: Dimension, const HIDDEN_LAYERS: usize>
+    TsMixer<L, C, DOut, HIDDEN_LAYERS>
+{
+    pub fn new(dropout_rate: f32) -> TsMixer<L, C, DOut, HIDDEN_LAYERS> {
+        return TsMixer {
+            last_layer: MixingLayer::new(false, 0.0),
+            layers: core::array::from_fn(|_| MixingLayer::new(true, dropout_rate)),
+        };
+    }
     pub fn forward<B: Dimension>(&mut self, mut x: Tensor<(B, L, C)>) -> Tensor<(B, L, DOut)> {
         for layer in self.layers.iter_mut() {
             x = layer.forward(x);
         }
         self.last_layer.forward(x)
+    }
+}
+#[cfg(test)]
+mod ts_mixer_tests {
+    use crate::dimensions::S;
+
+    use super::TsMixer;
+
+    #[test]
+    fn test_ts_mixer() {
+        let ts_mixer: TsMixer<S<5>, S<4>, S<1>, 6> = TsMixer::new(0.01);
+        println!("{:?}", ts_mixer);
     }
 }
